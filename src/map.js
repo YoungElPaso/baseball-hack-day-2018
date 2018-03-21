6a88022032da8545d02bc12617511d327ec7df63
Jesse Sutherland <jesse.a.sutherland@gmail.com>
2018-03-21 15:26:54 -0400

    trying to expand map component, but dont need to, should concentrate on map-direction component

--- src/map.js
+++ src/map.js
@@ -1,4 +1,12 @@
-import React, { Component } from 'react';
+/* eslint-disable no-undef */
+import React, { Component } from "react";
+const { compose, withProps, lifecycle } = require("recompose");
+const {
+  withScriptjs,
+  withGoogleMap,
+  GoogleMap,
+  Marker
+} = require("react-google-maps");
 // import { GoogleMap, Marker } from "react-google-maps"
 
 // const MyMapComponent = (props) =>
@@ -14,15 +22,52 @@ import React, { Component } from 'react';
 
 // export default MyMapComponent
 
-import { withScriptjs, withGoogleMap, GoogleMap, Marker } from "react-google-maps"
+// import {
+//   withScriptjs,
+//   withGoogleMap,
+//   GoogleMap,
+//   Marker
+// } from "react-google-maps";
 
-const MyMapComponent = withScriptjs(withGoogleMap((props) =>
-  <GoogleMap
-    defaultZoom={props.zoom || 8}
-    defaultCenter={props.center || { lat: -34.397, lng: 150.644 }}
-  >
-    {props.isMarkerShown && <Marker position={{ lat: -34.397, lng: 150.644 }} />}
-  </GoogleMap>
-))
+const MyMapComponent = compose(
+  withScriptjs(
+    withGoogleMap(
+      lifecycle({
+        componentDidMount() {
+          let GeoCodeService = new google.maps.Geocoder();
+          GeoCodeService.geocode(
+            {
+              address: this.props.origin
+            },
+            (result, status) => {
+              if (status === google.maps.GeocoderStatus.OK) {
+                this.setState(
+                  {
+                    origin: result
+                  },
+                  function() {
+                    console.log(result);
+                  }
+                );
+              } else {
+                console.error(`error fetching location ${result}`);
+              }
+            }
+          );
+        }
+      }),
+      props => (
+        <GoogleMap
+          defaultZoom={props.zoom || 8}
+          defaultCenter={props.center || { lat: -34.397, lng: 150.644 }}
+        >
+          {props.isMarkerShown && (
+            <Marker position={{ lat: -34.397, lng: 150.644 }} />
+          )}
+        </GoogleMap>
+      )
+    )
+  )
+);
 
-export default MyMapComponent
+export default MyMapComponent;
